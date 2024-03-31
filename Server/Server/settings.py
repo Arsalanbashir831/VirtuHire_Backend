815941e776aecf53d447e8e21e08e7e78f046d1f
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-04-01 01:42:52 +0500

    update cors

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -100,7 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://virtuhirebackend-production.up.railway.app",
 ]
 
 
